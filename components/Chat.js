f0061b2acbb181b0c69068de6e79a48a01c7cec8
j-mecham <80016860+j-mecham@users.noreply.github.com>
2023-11-06 11:36:30 -0700

    Added Gifted Chat functionality

--- components/Chat.js
+++ components/Chat.js
@@ -1,26 +1,73 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
   const { name } = route.params;
   const { color } = route.params;
+  
 
   useEffect(() => {
     navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: name + 'has joined the chat',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
 
+  // adds the new message to the displayed messages
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  // customizes sender bubbles to be black and received bubbles to be white
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
+
  return (
-   <View style={[styles.container, {backgroundColor: color}]}>
-     <Text>Hello {name}!</Text>
-   </View>
+  <View style={[styles.container, {backgroundColor: color}]}>
+    <GiftedChat
+    messages={messages}
+    renderBubble={renderBubble}
+    onSend={messages => onSend(messages)}
+    user={{
+      _id: 1
+    }}
+    />
+    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+  </View>
  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1
   }
 });
 
